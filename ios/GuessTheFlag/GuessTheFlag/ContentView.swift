1b6b93bcf28973bac00685217ac5cc1fd6c85600
Anh Mac <anhmv91@gmail.com>
2020-05-08 16:48:35 +0700

    animation guess the flag

--- ios/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ ios/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -48,8 +48,7 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer : Int = Int.random(in: 0...2)
     
-    @State private var showingScore : Bool = false
-    @State private var wrongMessage = ""
+    @State private var gameover : Bool = false
 
     @State private var score = 0
     
@@ -93,12 +92,22 @@ struct ContentView: View {
                     .animation(.easeIn)
                 }
             }
-        }
-        .alert(isPresented: $showingScore) {
-            Alert(title: Text("Gameover!"), message: Text(wrongMessage), dismissButton: .default(Text("Again")) {
-                self.askQuestion();
-                self.score = 0
-            })
+            
+            if gameover {
+                Color.black.opacity(0.7)
+                    .edgesIgnoringSafeArea(.all)
+                    .animation(nil)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .animation(.default)
+                
+                Text("Gameover!")
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 100)
+                    .background(Color.red).foregroundColor(.white)
+                    .transition(.asymmetric(insertion: .slide, removal: .slide))
+                    .animation(.default)
+            }
         }
     }
 
@@ -119,8 +128,14 @@ struct ContentView: View {
             score += 1
             self.askQuestion();
         } else {
-            wrongMessage = "Wrong! That's the flag of \(countries[number])"
-            showingScore = true
+            score = 0
+            gameover = true
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                withAnimation(.spring()) {
+                    self.gameover = false
+                }
+            }
         }
     }
 
