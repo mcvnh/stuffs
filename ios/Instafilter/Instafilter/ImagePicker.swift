6dd13c4d3489e16529767f07a7ec920f9cf95c6a
Anh Mac <anhmv91@gmail.com>
2020-05-18 18:25:11 +0700

    Instafilter

---
+++ ios/Instafilter/Instafilter/ImagePicker.swift
@@ -0,0 +1,44 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Mac Van Anh on 5/18/20.
+//  Copyright © 2020 Mac Van Anh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var image: UIImage?
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+}
---
+++ ios/Instafilter/Instafilter/ImageSaver.swift
@@ -0,0 +1,26 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Mac Van Anh on 5/18/20.
+//  Copyright © 2020 Mac Van Anh. All rights reserved.
+//
+
+import SwiftUI
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
